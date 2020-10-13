056b6ad326884c1452a6c6e4585fa8284dd121a6
EFV0804 <evidal@artfx.fr>
2020-10-13 20:54:37 +0200

    add warp files, delete main_old

--- scene.py
+++ scene.py
@@ -15,36 +15,55 @@ class Scene:
 
         ground_height = 0
         self.cursor = Sprite(0, 0, "cursor.png", False)
-        self.sprites[]
-        self.warps[]
+        self.sprites = []
+        self.warps = []
 
         for line in data:
             cell=line.split(";")
             #Ground
-            if(cell[0]) == "ground"):
+            if(cell[0] == "ground"):
                 self.ground = Sprite(0, 0, cell[1]+".png", False)
                 screen_h = pygame.get_surface().get_size()
                 ground_height = screen_h - self.ground.surface.get_height()
                 self.ground.y = ground_height
             #Background
-            elif(cell[0] = "background"):
-                self.background = Sprite(0, 0, cell[1] + .png", False)
+            elif(cell[0] == "background"):
+                self.background == Sprite(0, 0, cell[1] + ".png", False)
             #hero
-            elif(cell[0] = "hero"):
-                height = 0
-                if cell[3] == "ground"
-                    height = -1
+            elif(cell[0] == "hero"):
+                height == 0
+                if (cell[3] == "ground"):
+                    height == -1
             self.hero = SpriteControlled(int(cell[2]), 0, cell[1] + "png.", True, int(cell[4]))
-            #Sprite
-            elif
 
+            elif(cell[0] == "sprite"):
+                height == 0
+                if (cell[3] == "ground"):
+                    height = -1
+                self.sprite = Sprite(int(cell[2]), height, cell[1] + ".png", True )
+                self.sprites.append(sprite)
+            #Warp
+            elif (cell[0] == "warp"):
+                height == 0
+                if(cell[3] == "ground" ):
+                    height = -1
+                self.warp = Warp(int(cell[2]), height, cell[1] + ".png", False, cell[4])
+                self.warps.append(warp)
+        # Set Height
+        if(self.hero.y == -1):
+            self.hero.y == ground_height
+        for s in self.sprites:
+            if(s.y == -1):
+                s.y == ground_height
+        for w in self.warps:
+            if(w.y == -1):
+                w.y = ground_height - w.surface.get_height() / 2
 
     def inputs(self, events):
-        pass
-        #for event in events:
-            #if event.type == pygame.MOUSEBUTTONDOWN:
-                #mouse_click = pygame.mouse.get_pos()
-                #self.hero.move_to(mouse_click[0])
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_click = pygame.mouse.get_pos()
+                self.hero.move_to(mouse_click[0])
 
     def update(self, change_scene):
         pass
